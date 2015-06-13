441f7953dcd0501342a5aab6249d0b81e3dd00b0
Perceval FARAMAZ <perfaram@MacBook-Pro-de-Perceval.local>
2015-06-13 14:08:05 +0200

    Octofish function added

---
+++ octofish.fish
@@ -0,0 +1,25 @@
+
+function octofish
+  echo -e "
+               MMM.           .MMM
+               MMMMMMMMMMMMMMMMMMM
+               MMMMMMMMMMMMMMMMMMM      _____________
+              MMMMMMMMMMMMMMMMMMMMM    |             |
+             MMMMMMMMMMMMMMMMMMMMMMM   | FISH PLEASE |
+            MMMMMMMMMMMMMMMMMMMMMMMM   |_   _________|
+            MMMM::- -:::::::- -::MMMM    |/
+             MM~:~   ~:::::~   ~:~MM
+        .. MMMMM::. .:::+:::. .::MMMMM ..
+              .MM::::: ._. :::::MM.
+                 MMMM;:::::;MMMM
+          -MM        MMMMMMM
+          ^  M+     MMMMMMMMM
+              MMMMMMM MM MM MM
+                   MM MM MM MM
+                   MM MM MM MM
+                .~~MM~MM~MM~MM~~.
+             ~~~~MM:~MM~~~MM~:MM~~~~
+            ~~~~~~==~==~~~==~==~~~~~~
+             ~~~~~~==~==~==~==~~~~~~
+                 :~==~==~==~==~~"
+end
