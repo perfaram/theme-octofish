bf1f4b99d6ff346fd802d59259b7d52da51ecf11
Perceval FARAMAZ <perfaram@MacBook-Pro-de-Perceval.local>
2015-06-13 14:01:20 +0200

    Committed theme

---
+++ fish_greeting.fish
@@ -0,0 +1,13 @@
+function fish_greeting -d 'Write out the greeting'
+  if [ "$theme_display_disclaimer" = 'yes' ]
+    echo 'Unauthorized access or use of this computer system may subject violators to criminal, civil and/or administrative action.'
+  end
+  if [ "$theme_display_zen" = 'yes' ]
+    curl https://api.github.com/zen
+  end
+  set USERFULL (finger (id -un) | head -1 | cut -d: -f3-)
+  set UPTIME (uptime | awk '{sub(/[0-9]|user\,|users\,|load/, "", $6); sub(/mins,|min,/, "min", $6); sub(/user\,|users\,/, "", $5); sub(",", "min", $5); sub(":", "h ", $5); sub(/[0-9]/, "", $4); sub(/day,/, " day", $4); sub(/days,/, " days", $4); sub(/mins,|min,/, "min", $4); sub("hrs,", "h", $4); sub(":", "h ", $3); sub(",", "min", $3); print $3$4}')
+  set EXTIP (curl -s ipecho.net/plain ; echo)
+  echo -n 'Howdy,' & set_color $fish_color_user & echo -n "$USERFULL" & set_color $fish_color_normal & echo ' !'
+  echo 'System up since '"$UPTIME"', accessible on IP '"$EXTIP"
+end
